855154520131287b22a8d15ebea6a8fe935cf796
mrretakes1234 <christopherdj2010@gmail.com>
2025-06-04 16:54:37 +0000

    finish stp portfolio

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,23 +27,30 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    
+    
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
-
+   createPlatform(400, 700, 9, 200,"pink"); 
+   createPlatform(800, 700, 700, 70);
+   createPlatform(1590, 400, 90, 50, "green");
+createPlatform(400, 7, 100, 190);
     // TODO 3 - Create Collectables
 
+createCollectable("steve", 1350, 50);
+createCollectable("diamond", 200, 170, 5.5, 0.7);
 
-
-    
+    createCollectable("grace", 1350, 50);
+createCollectable("max", 200, 170, 0.5, 0.7);
+createCollectable("steve", 1350, 50);
     // TODO 4 - Create Cannons
+createCannon("top", 800, 100);
+createCannon("right", 300, 2000);
 
-
-    
+    createCannon("bottom", 900, 940);
+createCannon("left", 300, 2000)
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
